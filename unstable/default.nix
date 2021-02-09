8ccd0b625bbf8750a01f22bca626476a5e5dd538
Pacman99 <pachum99@gmail.com>
2021-02-09 10:49:26 -0700

    unstable: remove unused code

--- unstable/default.nix
+++ unstable/default.nix
@@ -8,24 +8,10 @@
   # packages pulled from master
   packages = pkgs: final: prev: {
     inherit (pkgs)
-      dhall
       discord
       element-desktop
       manix
-      nixpkgs-fmt
-      qutebrowser
-      signal-desktop
-      starship;
+      nixpkgs-fmt;
 
-    haskellPackages = prev.haskellPackages.override {
-      overrides = hfinal: hprev:
-        let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-        in
-        {
-          # same for haskell packages, matching ghc versions
-          inherit (pkgs.haskell.packages."ghc${version}")
-            haskell-language-server;
-        };
-    };
   };
 }
