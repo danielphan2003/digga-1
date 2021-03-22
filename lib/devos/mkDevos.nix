63a25ce819a281bf9f3c721ea24811fdca20a2cf
Pacman99 <pachum99@gmail.com>
2021-03-21 19:25:19 -0700

    init at using module system for mkDevos args, depends on nixpkgs master/unstable

--- lib/devos/mkDevos.nix
+++ lib/devos/mkDevos.nix
@@ -1,43 +1,95 @@
 { self, nixos, inputs, ... }:
 let
   devos = self;
+
+  argOpts = with nixos.lib; { config, ... }:
+    let
+      cfg = config;
+      mkPathOpt = name: mkOption {
+        type = types.path;
+        default = "${cfg.self}/${name}";
+        defaultText = "\${self}/${name}";
+      };
+      mkTypeOpt = name: type: mkOption {
+        type = with types; oneOf [ path type ];
+        default = import "${cfg.self}/${name}";
+        defaultText = "import \${self}/${name}";
+        apply = x: if builtins.isPath x then import x else x;
+      };
+
+      inputAttrs = types.functionTo types.attrs;
+    in
+    {
+      options = {
+        self = mkOption {
+          type = with types; addCheck attrs nixos.lib.isStorePath;
+          description = "The flake to create the devos outputs for";
+        };
+        hosts = mkPathOpt "hosts" // {
+          description = "Path to all host configurations";
+        };
+        packages = mkTypeOpt "pkgs"
+          (with types; functionTo inputAttrs) // {
+            description = "overlay for custom packages";
+          };
+        modules = mkTypeOpt "modules/module-list.nix" (with types; listOf anything) // {
+          description = "list of modules to include in confgurations";
+        };
+        profiles = mkPathOpt "profiles" // {
+          description = "path to profiles folder";
+        };
+        suites = mkTypeOpt "suites" inputAttrs // {
+          description = ''
+            function with inputs 'users' and 'profiles' that returns attribute 'system'
+            which defines suites passed to configurations as the suites specialArg
+          '';
+        };
+        users = mkPathOpt "users" // {
+          description = "path to folder containing user profiles";
+        };
+        extern = mkTypeOpt "extern" inputAttrs // {
+          description = ''
+            function with input 'inputs' with all devos and this flake's inputs
+            that returns modules, overlays, and specialArgs to be included across devos
+          '';
+        };
+        overlays = mkPathOpt "overlays" // {
+          description = "path to folder containing overlays which will be applied to pkgs";
+        };
+        overrides = mkTypeOpt "overrides" types.attrs // {
+          description = "attrset of packages and modules that will be pulled from nixpkgs master";
+        };
+      };
+    };
 in
 
-{ self
-, hosts ? "${self}/hosts"
-, packages ? "${self}/pkgs"
-, modules ? import "${self}/modules/module-list.nix"
-, profiles ? "${self}/profiles"
-, suites ? import "${self}/suites"
-, users ? "${self}/users"
-, extern ? import "${self}/extern"
-, overlays ? "${self}/overlays"
-, overrides ? import "${self}/overrides"
-, ...
-}:
+{ ... } @ args:
 let
   inherit (self) lib;
   inherit (lib) os;
 
   inherit (inputs) utils deploy;
 
-  external = extern { inherit inputs; };
+  cfg = (nixos.lib.evalModules { modules = [ args argOpts ]; }).config;
 
-  multiPkgs = os.mkPkgs { inherit overrides; extern = external; };
+  external = cfg.extern { inputs = inputs // self.inputs; };
 
-  allSuites = os.mkSuites { inherit profiles users suites; };
+  multiPkgs = os.mkPkgs { inherit (cfg) overrides; extern = external; };
+
+  allSuites = os.mkSuites { inherit (cfg) profiles users suites; };
 
   outputs = {
     nixosConfigurations = os.mkHosts {
-      inherit multiPkgs overrides;
+      inherit multiPkgs;
+      inherit (cfg) overrides;
       extern = external;
       suites = allSuites;
-      dir = hosts;
+      dir = cfg.hosts;
     };
 
-    nixosModules = lib.pathsToImportedAttrs modules;
+    nixosModules = lib.pathsToImportedAttrs cfg.modules;
 
-    overlay = import packages;
+    overlay = cfg.packages;
     #overlays = lib.pathsToImportedAttrs (lib.pathsIn (builtins.toPath overlays));
     overlays = lib.pathsToImportedAttrs (lib.pathsIn ../../overlays);
 
